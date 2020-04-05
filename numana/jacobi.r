41fd0218ceaf96fbf34e97433eba8b99e786dab2
covid1974 <arnabc74@gmail.com>
2020-04-05 17:38:36 +0530

    silly

---
+++ numana/jacobi.r
@@ -0,0 +1,21 @@
+jacobi = function(A) {
+
+    n = nrow(A)
+
+    mx = abs(A[2,1])
+    mi = 2; mj = 1
+    for(i in 2:n) {
+        for(j in 1:(i-1)) {
+            if(mx < abs(A[i,j])) {
+                mi = i; mj = j; mx = abs(A[i,j]);
+            }
+        }
+    }
+    cat(mx,"\n")
+    theta = atan2(2*A[mi,mj], A[mj,mj]-A[mi,mi])/2
+
+    sn = sin(theta); cs = cos(theta)
+    G = diag(n); G[c(mi,mj),c(mi,mj)] = c(cs,sn,-sn,cs)
+    
+    G %*% A %*% t(G)
+}
