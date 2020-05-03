9a04c6e921970fd14279ca0c6dcff34116fa76fc
covid1974 <arnabc74@gmail.com>
2020-05-03 19:25:54 +0530

    silly

---
+++ index.rb
@@ -0,0 +1,61 @@
+<NOTE>
+@{
+<HEAD1>COVIDeo lectures</HEAD1>
+Here is a collection of all my video lectures.
+
+<HEAD2>For ISI, Tezpur students</HEAD2>
+<UL>
+<LI><LINK to="https://drive.google.com/open?id=10zqWhG8QmW6HRp5D3KfkZOzhiEq2eyT1">Simulation</LINK></LI>
+<LI><LINK to="https://drive.google.com/open?id=13WI6Cb7XScFaTF2qn9i9o6jknUu7_H-r">PCA</LINK></LI>
+<LI><LINK to="https://drive.google.com/open?id=172Ymg4-kOZGtqj9HtvEAy2obqoyC8x1Z">CART</LINK></LI>
+</UL>
+<HEAD2>For ISI, Kolkata students</HEAD2>
+<UL>
+<LI>Nonlinear equations: [
+<LINK to="https://drive.google.com/open?id=12DFz3rbU5Uk57HNB9qphwkvGmCmoG1wt">1</LINK> | 
+<LINK to="https://drive.google.com/open?id=12EqfoU6aypE2VZeMU4Kl9OT2HLhd_2EH">2</LINK> | 
+<LINK to="https://drive.google.com/open?id=12IemezqLygftvnke7C8PYAY5ZljYkxWL">3</LINK> | 
+<LINK to="https://drive.google.com/open?id=1etRykt-PGboAhKQMVIwS0_ZEzvHXN-HL">4</LINK> |
+<LINK to="https://drive.google.com/open?id=16p5tNjAdITaqE9890Fo5BKkpO9s5N4o7">5</LINK> ]
+</LI>
+<LI>Matrix algorithms: [
+<LINK to="https://drive.google.com/open?id=19dYgN9aWKABSFtQ4vwVAcmDQwzFZz8I5">1</LINK> | 
+<LINK to="https://drive.google.com/open?id=1605Z140pk0noRfLX_RuNlaACTQRhTHuV">2</LINK> | 
+<LINK to="https://drive.google.com/open?id=1G9ZWfxTtZ7TXFxXbO0TopxxsClnhHvi3">3</LINK> | 
+<LINK to="https://drive.google.com/open?id=1fD0qj15vJgHI-Iha1gwYWmjXXHABsR1_">4</LINK> | 
+<LINK to="https://drive.google.com/open?id=1HvRfl1gY7zRFJXuED9TqwLYSypyV0GWg">5</LINK> ]
+</LI>
+<LI>Differential equations: [
+<LINK to="https://drive.google.com/open?id=1ar4hMYd45PqHAzeA4gh3vtM7HNmNiU67">1</LINK> | 
+<LINK to="https://drive.google.com/open?id=1l6Yx1c-JM4orRDxuPwk52Or7c4gWLWgQ">2</LINK> | 
+<LINK to="https://drive.google.com/open?id=1W1O3xzP3Ymezv6qpdA3Z1CYMtB5c0kWp">3</LINK> ]
+</LI>
+<LI>Numerical integration: [
+<LINK to="https://drive.google.com/open?id=1X-KfSGlER6bOSdz6liS2WBNxW9gZHkpu">1</LINK> | 
+<LINK to="https://drive.google.com/open?id=1eFZThFqUby-SEAlQaU93fJMh64IFIs2G">2</LINK> | 
+<LINK to="https://drive.google.com/open?id=1Egg0aq8VKPlv-nTzdu2-wGyIleIxQlMH">3</LINK> | 
+<LINK to="https://drive.google.com/open?id=1W1JQbFacalGqGif9BoF2tF-4arVRtfE5">4</LINK> ]
+</LI>
+<LI>Interpolation: [
+<LINK to="https://drive.google.com/open?id=1wd7SDwzsquTLUly-Mjs0iidRW4428LL7">1</LINK> ]
+</LI>
+<LI>Error analysis: [
+<LINK to="https://drive.google.com/open?id=1YFy_IpF4wpR-VgN0xz6t_P4pLKU3Ly3f">1</LINK> ]
+</LI>
+</UL>
+<HEAD2>For general interest</HEAD2>
+<UL>
+<LI>Probability distributions: [
+<LINK to="https://drive.google.com/open?id=1IBZlQpn8CVdMlN1ojYQxZPtRTb7X024X">1</LINK> ]
+</LI>
+<LI>Riemann integration: [
+<LINK to="https://drive.google.com/open?id=1o19wibyvrrQz0ysOIx-kIF-L20WLWJ7L">1</LINK> |
+<LINK to="https://drive.google.com/open?id=1cu0QTMgy7QJg6UC2aQIxffOKpafNMOqg">2</LINK> ]
+</LI>
+<LI>Multivariate calculus: [
+<LINK to="https://drive.google.com/open?id=130I4eeKH-CdukoQXGNvgOdPltAnecfl_">1</LINK> ]
+</LI>
+
+</UL>
+@}
+</NOTE>
